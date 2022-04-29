1ad3230a01550b3dbd027d23927fd70f9a79faab
AbuHurairah127 <abuhurairahMuhammadShafique@gmail.com>
2022-04-29 10:16:01 +0500

    Navbar Completed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
 import React from "react";
+import Button from "./components/button/Button";
+import Header from "./components/header/Header";
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Header />
+      <div className="m-4">
+        <Button />
+      </div>
+    </div>
+  );
 }
 
 export default App;
