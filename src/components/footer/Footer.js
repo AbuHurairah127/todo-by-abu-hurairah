22df55b16b1676f2b8751247ac358f92472e21b4
AbuHurairah127 <abuhurairahMuhammadShafique@gmail.com>
2022-05-02 15:25:03 +0500

    footer styling completed

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,18 @@
+import React from "react";
+import UseFooter from "./UseFooter";
+
+const Footer = () => {
+  const { year } = UseFooter();
+  return (
+    <div className="justify-center w-screen flex-grow flex-shrink-0 flex items-center h-12 bg-indigo-500">
+      <span className="text-white px-2">
+        &copy; {year} All rights reserved.
+      </span>
+      <a href="/" className="font-bold text-white">
+        www.abuhurairah.com
+      </a>
+    </div>
+  );
+};
+
+export default Footer;
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,18 +1,20 @@
 import React from "react";
 import Button from "../../components/button/Button";
 import DayDetails from "../../components/dayDetails/DayDetails";
+import Footer from "../../components/footer/Footer";
 import Header from "../../components/header/Header";
-import OptionBar from "../../components/sidebars/optionBar-right/OptionBar";
 
 const Home = () => {
   return (
-    <div className="flex w-full flex-col content-center">
+    <div className="flex w-full flex-col h-screen content-center">
       <Header />
       <DayDetails />
-      <div className="block m-auto">
+      <div className="block mx-auto">
         <Button label="Add a TASK" />
       </div>
-      <OptionBar />
+      <div className="mt-auto">
+        <Footer className="" />
+      </div>
     </div>
   );
 };
